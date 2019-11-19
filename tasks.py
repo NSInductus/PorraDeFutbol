9488845b1c1cf7706aa1686f0911721e39464088
Solano96 <fransol0728@gmail.com>
2019-11-19 11:51:24 +0100

    Corregido error en tests

--- tasks.py
+++ tasks.py
@@ -1,6 +1,19 @@
 from invoke import task, run
+@task
+def install(c):
+    print("Instalando paquetes...")
+    c.run("pip install -r requirements.txt")
+    print("Instalación finalizada.")
+
 
 @task
 def test(c):
+    print("Ejecutando tests...")
     c.run("pytest -q tests/test_*.py")
-    c.run("pytest --cov=./") 
+    print("Ejecución de tests finalizada.")
+
+@task
+def coverage(c):
+    print("Test de cobertura...")
+    c.run("pytest --cov=Mercado --cov=Portfolio tests/")
+    print("Test de cobertura finalizado.")
